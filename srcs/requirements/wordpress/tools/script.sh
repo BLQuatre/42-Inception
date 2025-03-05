16a7a3b6f3232d7036dfe2503834992db6c9b06e
BL4 <clem29auv@gmail.com>
2025-03-05 06:47:24 +0100

    ~ | Add WORKDIR to clean script.sh

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-cd /var/www/wordpress
-
-export HTTP_HOST="cauvray.42.fr"
+export HTTP_HOST=${DOMAIN_NAME}
 
 while ! mariadb -h mariadb -u ${WP_SQL_USER} -p${WP_SQL_PASSWORD} -e "SELECT 1" &>/dev/null; do
 	sleep 2
@@ -17,7 +15,6 @@ if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
 		--dbuser=${WP_SQL_USER} \
 		--dbpass=${WP_SQL_PASSWORD} \
 		--dbhost=mariadb \
-		--path='/var/www/wordpress' \
 		--locale=fr_FR
 fi
 
@@ -29,49 +26,42 @@ if ! wp core is-installed; then
 		--admin_password=${WP_ADMIN_PASSWORD} \
 		--admin_email=${WP_ADMIN_EMAIL} \
 		--skip-email \
-		--allow-root \
-		--path='/var/www/wordpress'
+		--allow-root
 fi
 
 if ! wp user exists ${WP_USER_USER}; then
 	wp user create ${WP_USER_USER} ${WP_USER_EMAIL} \
 		--role=author \
 		--user_pass=${WP_USER_PASSWORD} \
-		--allow-root \
-		--path='/var/www/wordpress'
+		--allow-root
 fi
 
 if ! wp theme is-installed astra; then
 	wp theme install astra \
-		--allow-root \
-		--path='/var/www/wordpress'
+		--allow-root
 fi
 
 if ! wp theme is-active astra; then
 	wp theme activate astra \
-		--allow-root \
-		--path='/var/www/wordpress'
+		--allow-root
 fi
 
-wp config has WP_REDIS_HOST || wp config set WP_REDIS_HOST "redis" --path=/var/www/wordpress
-wp config has WP_REDIS_PORT || wp config set WP_REDIS_PORT 6379 --raw --path=/var/www/wordpress
-wp config has WP_CACHE || wp config set WP_CACHE true --raw --path=/var/www/wordpress
+wp config has WP_REDIS_HOST || wp config set WP_REDIS_HOST "redis"
+wp config has WP_REDIS_PORT || wp config set WP_REDIS_PORT 6379 --raw
+wp config has WP_CACHE || wp config set WP_CACHE true --raw
 
 if ! wp plugin is-installed redis-cache; then
 	wp plugin install redis-cache \
-		--allow-root \
-		--path='/var/www/wordpress'
+		--allow-root
 fi
 
 if ! wp plugin is-active redis-cache; then
 	wp plugin activate redis-cache \
-		--allow-root \
-		--path='/var/www/wordpress'
+		--allow-root
 fi
 
 wp redis enable \
-	--allow-root \
-	--path='/var/www/wordpress'
+	--allow-root
 
 mkdir -p /run/php
 
