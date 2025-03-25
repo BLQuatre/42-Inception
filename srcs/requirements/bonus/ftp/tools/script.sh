46080d34229144db8383adcd956a3665aa72870b
BL4 <clem29auv@gmail.com>
2025-03-25 11:45:55 +0100

    ~ | Finished ftp

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -4,9 +4,13 @@ set -e
 
 adduser -D -h /home/ftp -s /sbin/nologin ${FTP_USER}
 
-echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd \
+echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
 echo "${FTP_USER}" >> /etc/vsftpd/allowed_users
 
 chown -R ${FTP_USER}:${FTP_USER} /home/ftp
+chmod a-w /home/ftp
+
+touch /var/log/vsftpd.log
+chmod 644 /var/log/vsftpd.log
 
 exec vsftpd /etc/vsftpd/vsftpd.conf
