98e18683c9d92b3209918e4805157a0707b24679
BL4 <clem29auv@gmail.com>
2025-04-04 00:12:23 +0200

    ~ | Secure ftp adduser

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-adduser -D -h /home/ftp -s /sbin/nologin ${FTP_USER}
+id ${FTP_USER} &>/dev/null || adduser -D -h /home/ftp -s /sbin/nologin ${FTP_USER}
 
 echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
 echo "${FTP_USER}" >> /etc/vsftpd/allowed_users
